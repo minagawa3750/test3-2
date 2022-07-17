b43aa15925c4e2d510154d0546f45e04ee6371e6
EC2 Default User <ec2-user@ip-172-31-43-251.us-west-2.compute.internal>
2022-07-17 17:57:36 +0000

    じゃんけんアプリ提出

--- jyanken.rb
+++ jyanken.rb
@@ -1,42 +1,43 @@
 def jyanken_game
-  puts "じゃんけん..."
+  puts"じゃんけん"
   puts "0(グー)1(チョキ)2(パー)3(戦わない)"
-
+  
+  jyanken = ["グー","チョキ","パー"]
   player_hand = gets.to_i
   program_hand = rand(3)
 
-if player_hand > 3
-  puts"じゃんけんアプリを修了します"
-  return false
-end
+  if player_hand > 2
+    puts"じゃんけんアプリを終わらすぜ！"
+    return false
+  end
 
- jyanken = ["グー","チョキ","パー"]
-  
-  puts "ホイ!"
-  puts "----------------"
-  puts "あなた:#{jyanken[player_hand]}をだしました"
-  puts "相手：#{jyanken[program_hand]}をだしました"
-  puts "----------------"
+  if player_hand <= 2
+    puts "ホイ!"
+    puts "----------------"
+    puts "あなた:#{jyanken[player_hand]}をだしました"
+    puts "相手：#{jyanken[program_hand]}をだしました"
+    puts "----------------"
+  end
 
   if (player_hand == 0 && program_hand == 1) ||
      (player_hand == 1 && program_hand == 2) ||
      (player_hand == 2 && program_hand == 0)
- 
-     jyanken = "win"
-     return false
+     
+      @jyanken_result = "win"
+      attimuite_hoi
       
   elsif player_hand == program_hand
-  
      puts "あいこで..."
-     return true
+     jyanken_game
   
   else 
-     jyanken = "lost"
-     return false
+     @jyanken_result = "lost"
+     attimuite_hoi
 
   end
 end
-
+ 
+def attimuite_hoi
   puts"あっち向いて..."
   puts"0(上)1(下)2(左)3(右)"
   code =["上","下","左","右"]
@@ -48,13 +49,16 @@ end
   puts"相手:#{code[program_code]}"
   puts"----------------"
 
-if jyanken == "win" && player_code == program_code
-  puts "あなたの勝ち！"
+  if  @jyanken_result == "win" && player_code == program_code
+    puts "勝ったぜ！"
   
-elsif jyanken =="lost" && player_code == program_code
-  puts "あなたの負け"
-
-else
-  jyanken_game
-
+  elsif @jyanken_result == "lost" && player_code == program_code
+    puts "負けたぜ..."
+  
+  else
+    jyanken_game
+  
+  end
 end
+
+jyanken_game
